b691b98dc7484ecd5c709e095d594f29a6486d8e
wan <pyunwan888@naver.com>
2022-09-14 17:38:25 +0900

    drill 04 과제 제출

---
+++ 04/draw_circle.py
@@ -0,0 +1,41 @@
+import turtle
+
+
+count =5
+while(count>0):
+    turtle.forward(500)
+    turtle.left(90)
+    count=count-1
+
+count=5
+while(count>0):
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(500)
+    turtle.right(90)
+    turtle.forward(100)
+    turtle.right(90)
+    turtle.forward(500)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(500)
+    turtle.right(90)
+    turtle.forward(100)
+    turtle.right(90)
+    turtle.forward(500)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.left(90)
+    count=count-1
+    
+
+
+
+
+
+
+
+
+
+turtle.exitonclick()
